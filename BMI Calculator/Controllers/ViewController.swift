a77407e8ae6ca947197957763975552aa55e5271
dessanagit <dessanacms@gmail.com>
2021-09-09 22:54:13 -0400

    Configure UISlider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+                    
+                    ////round the number - two decimal places
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+        
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+                    
+                    ////round the number - no decimal places
+        let weight = String(format: "%.f", sender.value)
+        weightLabel.text = "\(weight)kg"
     }
 
 
