9646fda52fcece926d50af8734d4cbd1e4a896ac
Dessana Santos <dessanacms@gmail.com>
2021-11-10 13:28:17 +0000

    Final Commit

--- BMI Calculator/Views/CalculatorBrain.swift
+++ BMI Calculator/Views/CalculatorBrain.swift
@@ -6,20 +6,41 @@
 //  Copyright © 2021 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    
-    var bmi: Float = 0.0
+  
+    var bmi: BMI?
     
     func getBMIValue() -> String {
         
-        var bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
     }
     
-    mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight/pow(height, 2)
-      
+    func getAdvice() -> String {
+        return bmi?.advice ?? "There's no Advice"
     }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.systemRed
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        let bmiValue = weight/pow(height, 2)
+      
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.systemIndigo)
+            print("Underweight")
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor.green)
+            print("Normal weight")
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.magenta)
+            print("Overweight")
+        }
+        
+      
+}
+    
 }
--- BMI Calculator/Views/ResultViewController.swift
+++ BMI Calculator/Views/ResultViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 class ResultViewController: UIViewController {
 
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
     
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bgColor: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        bgColor.backgroundColor = color
+//        view.backgroundColor = color
+        //// in case that bgColor wasn't created
+        
 
         // Do any additional setup after loading the view.
     }
